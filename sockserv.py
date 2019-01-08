f4b16dc7082970ef513c6662dbe2f935ec4a77c3
ZeroDelayy <nilsfv@gmx.co.uk>
2019-01-08 21:31:28 +0000

    Started revamping of server code, allowing for multiple connections.

--- sockserv.py
+++ sockserv.py
@@ -2,14 +2,23 @@
 
 import socket
 import sys
-import _thread
 import ssl
 import os
 import subprocess
 from datetime import datetime
+import selectors
+import traceback
+
+
 
 HOST = ''
 PORT = 27888
+
+sel = selectors.DefaultSelector()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Avoid bind() exception: OSError: [Errno 48] Address already in use
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 serverstate = False
 updatestate = False
@@ -193,39 +202,42 @@ else:
     with open("Transactions.txt", "w") as w1:
         w1.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ARK Socket Server has been started.")
 
-context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.load_cert_chain(certfile="crt/cert-20170526-150709.crt", keyfile="crt/key-20170526-150709.pem")
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("Socket created")
-with open("Transactions.txt", "a") as w1:
-    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Socket created")
 
-try:
-    s.bind((HOST, PORT))
-except socket.error as msg:
-    print("Bind failed. Error Code : " + str(msg[0]) + " Message " + msg[1])
-    with open("Transactions.txt", "a") as w1:
-        w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Bind failed. Error Code : " + str(msg[0]) + " Message " + msg[1])
-    sys.exit()
-
-print("Socket bind complete")
-with open("Transactions.txt", "a") as w1:
-    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Socket bind complete")
-
-s.listen(1)
-print("Socket now listening")
-with open("Transactions.txt", "a") as w1:
-    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Socket now listening")
-
-
-while 1:
-
-    newsocket, fromaddr = s.accept()
-    print("Connected with " + fromaddr[0] + ":" + str(fromaddr[1]))
-    with open("Transactions.txt", "a") as w1:
-        w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Connected with " + fromaddr[0] + ":" + str(fromaddr[1]))
-    connstream = context.wrap_socket(newsocket, server_side=True, do_handshake_on_connect=True)
-    _thread.start_new_thread(clientthread ,(connstream,))
-s.close()
+# context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+# context.load_cert_chain(certfile="crt/cert-20170526-150709.crt", keyfile="crt/key-20170526-150709.pem")
+#
+#
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# print("Socket created")
+# with open("Transactions.txt", "a") as w1:
+#     w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Socket created")
+#
+# try:
+#     s.bind((HOST, PORT))
+# except socket.error as msg:
+#     print("Bind failed. Error Code : " + str(msg[0]) + " Message " + msg[1])
+#     with open("Transactions.txt", "a") as w1:
+#         w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Bind failed. Error Code : " + str(msg[0]) + " Message " + msg[1])
+#     sys.exit()
+#
+# print("Socket bind complete")
+# with open("Transactions.txt", "a") as w1:
+#     w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Socket bind complete")
+#
+# s.listen(1)
+# print("Socket now listening")
+# with open("Transactions.txt", "a") as w1:
+#     w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Socket now listening")
+#
+#
+# while 1:
+#
+#     newsocket, fromaddr = s.accept()
+#     print("Connected with " + fromaddr[0] + ":" + str(fromaddr[1]))
+#     with open("Transactions.txt", "a") as w1:
+#         w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Connected with " + fromaddr[0] + ":" + str(fromaddr[1]))
+#     connstream = context.wrap_socket(newsocket, server_side=True, do_handshake_on_connect=True)
+#     _thread.start_new_thread(clientthread ,(connstream,))
+# s.close()
