0320647d1e365eb47bb10bd79eac27d94409ad6c
ZeroDelayy <nilsfv@gmx.co.uk>
2019-01-08 20:22:26 +0000

    Version 0.3.2 as of 2016, this signals start of the 2019 Refresh

---
+++ sockcli.py
@@ -0,0 +1,147 @@
+# Version 0.3.2
+
+import socket
+import sys
+import encodings
+import os
+import ssl
+from colorama import Fore, Back, Style
+from colorama import init
+
+init()
+if os.path.isfile("Errors.txt") is True:
+    sys.stderr = open("Errors.log", "a")
+else:
+    sys.stderr = open("Errors.log", "w")
+
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+context.verify_mode = ssl.CERT_REQUIRED
+context.check_hostname = True
+context.load_verify_locations("crt/cert-20170526-150709.crt")
+
+while True:
+    print("Welcome to the ARK Server Remote Management Client!")
+    print("---------------------------------------------------")
+    print("Version 0.3.2")
+    print("")
+    while True:
+        print("Please select one of the following options:")
+        print("")
+        print("1. Start ARK Server - The Center")
+        print("2. Start ARK Server - Scorched Earth")
+        print("")
+        print("3. Stop ARK Server")
+        print("")
+        print("4. Update ARK Server - The Center")
+        print("5. Update ARK Server - Scorched Earth")
+        print("")
+        print("6. Check status of ARK server")
+        print("")
+        print("")
+        usrin = input("Option:  ")
+
+        # Start The Center
+        if usrin == "1":
+            conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname="ark.com")
+            conn.connect(("cbm.selfhost.eu", 27888))
+            print(Fore.GREEN + conn.recv(1024).decode("utf-8"))
+            conn.send(b"start1")
+            print("")
+            print(Fore.GREEN + conn.recv(1024).decode("utf-8"))
+            print(Fore.RESET)
+            conn.close()
+
+        # Start Scorched Earth
+        if usrin == "2":
+            conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname="ark.com")
+            conn.connect(("cbm.selfhost.eu", 27888))
+            print(Fore.GREEN + conn.recv(1024).decode("utf-8"))
+            conn.send(b"start2")
+            print("")
+            print(Fore.RED + conn.recv(1024).decode("utf-8"))
+            print(Fore.RESET)
+            conn.close()
+
+        # Stop server
+        if usrin == "3":
+            conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname="ark.com")
+            conn.connect(("cbm.selfhost.eu", 27888))
+            print(Fore.GREEN + conn.recv(1024).decode("utf-8"))
+            conn.send(b"stop")
+            print("")
+            print(Fore.RED + conn.recv(1024).decode("utf-8"))
+            print(Fore.RESET)
+            conn.close()
+
+        # Update The Center
+        if usrin == "4":
+            conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname="ark.com")
+            conn.connect(("cbm.selfhost.eu", 27888))
+            print(Fore.GREEN + conn.recv(1024).decode("utf-8"))
+            conn.send(b"update1")
+            print("")
+            print(Fore.BLUE + conn.recv(1024).decode("utf-8"))
+            print(Fore.RESET)
+            conn.close()
+
+        # Update Scorched Earth
+        if usrin == "5":
+            conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname="ark.com")
+            conn.connect(("cbm.selfhost.eu", 27888))
+            print(Fore.GREEN + conn.recv(1024).decode("utf-8"))
+            conn.send(b"update2")
+            print("")
+            print(Fore.BLUE + conn.recv(1024).decode("utf-8"))
+            print(Fore.RESET)
+            conn.close()
+
+        # Status check
+        if usrin == "6":
+            conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname="ark.com")
+            conn.connect(("cbm.selfhost.eu", 27888))
+            print(Fore.GREEN + conn.recv(1024).decode("utf-8"))
+            conn.send(b"status")
+            print("")
+            print(Fore.YELLOW + conn.recv(1024).decode("utf-8"))
+            print(Fore.RESET)
+            conn.close()
+
+        if usrin == "7":
+            print("                {")
+            print('             }   }   {')
+            print('            {   {  }  }')
+            print('             }   }{  {')
+            print('           _{  }{  }  }_')
+            print('          (  }{  }{  {  )')
+            print('          |""---------""| ')
+            print('          |             /""\\')
+            print('          |            | _  |')
+            print('          |             / | |')
+            print('          |             |/  |')
+            print('          |             /  / ')
+            print('          |            |  / ')
+            print('          |            "T"')
+            print('           ""---------""')
+            print('COFFEE')
+            print('IS')
+            print('SERVED!')
+            print('')
+        if usrin == "8":
+            print('             .---------------------------.')
+            print('            /_   _   _         __  __   /|')
+            print('           // \ / \ / \ |_/ | |_  (_   / |')
+            print('          / \_  \_/ \_/ | \ | |__ ,_/ /  |')
+            print('         :.__________________________/   /')
+            print('         |  .--. .--.   .--.   .--.  |  /')
+            print('         | (    )    ) (    ) (    ) | /')
+            print("         |  '--' '--'   '--'   '--'  |/")
+            print("         '---------------------------' ")
+            print('YOUR COOKIES HAVE BEEN DONATED TO A CHARITY.')
+            print('')
+            print('THANK YOU FOR YOUR CHARITABLE DONATION.')
+            print('')
+
+
+
+
---
+++ sockserv.py
@@ -0,0 +1,231 @@
+# Version 0.3.2
+
+import socket
+import sys
+import _thread
+import ssl
+import os
+import subprocess
+from datetime import datetime
+
+HOST = ''
+PORT = 27888
+
+serverstate = False
+updatestate = False
+
+def serverstatus():
+    global serverstate
+    tasklistr = os.popen("tasklist").read()
+    if "ShooterGameServer.exe" in tasklistr:
+        serverstate = True
+        print("Server Status is Running")
+        with open("Transactions.txt", "a") as w1:
+            w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Server Status is Running")
+    else:
+        serverstate = False
+        print("Server Status is Stopped")
+        with open("Transactions.txt", "a") as w1:
+            w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Server Status is Stopped")
+
+def updatestatus():
+    global updatestate
+    tasklistr = os.popen("tasklist").read()
+    if "steamcmd.exe" in tasklistr:
+        updatestate = True
+        print("Update in progress.")
+        with open("Transactions.txt", "a") as w1:
+            w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Update in progress")
+    else:
+        updatestate = False
+        print("Update finished.")
+        with open("Transactions.txt", "a") as w1:
+            w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Update stopped")
+
+
+def clientthread(connstream):
+    connstream.send(b'Welcome to the server! We are running version 0.3.2.')
+
+    while True:
+        data = connstream.recv(1024)
+        global serverstate
+
+        # Start The Center
+        if data == (b"start1"):
+            serverstatus()
+            updatestatus()
+            if serverstate is False and updatestate is False:
+                print("Starting Server The Center")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Starting Server The Center")
+                subprocess.Popen(
+                    'C:\ARK\ARKServer 3\ShooterGame\Binaries\Win64\ShooterGameServer.exe "TheCenter?MaxPlayers=20?listen" -log -NoBattlEye')
+                connstream.sendall(b"ARK Server - The Center is being started")
+            elif serverstate == True:
+                print("Server is already running")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Server is already running")
+                connstream.sendall(b"Server is already running")
+            elif updatestate == True:
+                print("Update is still in progress")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Update is still in progress")
+                connstream.sendall(b"Server update is still in progress")
+
+        # Start Scorched Earth
+        elif data == (b"start2"):
+            serverstatus()
+            updatestatus()
+            if serverstate is False and updatestate is False:
+                print("Starting Server Scorched Earth")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Starting Server Scorched Earth")
+                subprocess.Popen(
+                    'C:\ARK\ARKServer 2\ShooterGame\Binaries\Win64\ShooterGameServer.exe "ScorchedEarth_P?MaxPlayers=20?listen" -log -NoBattlEye')
+                connstream.sendall(b"ARK Server - Scorched Earth is being started")
+            elif serverstate == True:
+                print("Server is already running")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Server is already running")
+                connstream.sendall(b"Server is already running")
+            elif updatestate == True:
+                print("Update is still in progress")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Update is still in progress")
+                connstream.sendall(b"Server update is still in progress")
+
+        # Stop server
+        elif data == (b"stop"):
+            serverstatus()
+            updatestatus()
+            if serverstate == False:
+                print("Server is already stopped")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Server is already stopped")
+                connstream.sendall(b"Server is already stopped")
+            elif serverstate == True:
+                print("Server is being stopped")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Server is being stopped")
+                os.system("taskkill /im ShooterGameServer.exe /f")
+                connstream.sendall(b"Server is being stopped")
+
+        # Update The Center
+        elif data == (b"update1"):
+            serverstatus()
+            updatestatus()
+            if serverstate is False and updatestate is False:
+                print("Updating Server The Center")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Updating Server The Center")
+                subprocess.Popen('C:\ARK\ARK Server Launcher\SteamCMD\SteamCMD.exe +runscript tc.txt')
+                connstream.sendall(b"ARK Server - The Center is being updated")
+            elif serverstate == True:
+                print("Server is still running. Cannot update.")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime(
+                        "%Y-%m-%d %H:%M:%S") + " Server is still running. Cannot update.")
+                connstream.sendall(b"Server needs to be stopped in order to update it!")
+            elif updatestate == True:
+                print("Another update is still running. Cannot continue.")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime(
+                        "%Y-%m-%d %H:%M:%S") + " Another update is still running. Cannot continue.")
+                connstream.sendall(b"Another update is already in progress!")
+
+        # Update Scorched Earth
+        elif data == (b"update2"):
+            serverstatus()
+            updatestatus()
+            if serverstate is False and updatestate is False:
+                print("Updating Server Scorched Earth")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Updating Server Scorched Earth")
+                subprocess.Popen('C:\ARK\ARK Server Launcher\SteamCMD\SteamCMD.exe +runscript se.txt')
+                connstream.sendall(b"ARK Server - Scorched Earth is being updated")
+            elif serverstate == True:
+                print("Server is still running. Cannot update.")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime(
+                        "%Y-%m-%d %H:%M:%S") + " Server is still running. Cannot update.")
+                connstream.sendall(b"Server needs to be stopped in order to update it!")
+            elif updatestate == True:
+                print("Another update is still running. Cannot continue.")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime(
+                        "%Y-%m-%d %H:%M:%S") + " Another update is still running. Cannot continue.")
+                connstream.sendall(b"Another update is already in progress!")
+
+        # Status check
+        elif data == (b"status"):
+            serverstatus()
+            updatestatus()
+            if updatestate == True:
+                print("Update in progress.")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Update in progress.")
+                connstream.sendall(b"Update in progress...")
+            elif serverstate == False:
+                print("Server is stopped")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Server is stopped")
+                connstream.sendall(b"Server is stopped.")
+            elif serverstate == True:
+                print("Server is running")
+                with open("Transactions.txt", "a") as w1:
+                    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Server is running")
+                connstream.sendall(b"Server is running.")
+
+        if not data:
+            break
+
+    connstream.close()
+    print("Connection with " + fromaddr[0] + ":" + str(fromaddr[1]) + " closed")
+    with open("Transactions.txt", "a") as w1:
+        w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Connection with " + fromaddr[0] + ":" + str(
+            fromaddr[1]) + " closed")
+
+
+if os.path.isfile("Transactions.txt") is True:
+    with open("Transactions.txt", "a") as w1:
+        w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ARK Socket Server has been started.")
+else:
+    with open("Transactions.txt", "w") as w1:
+        w1.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ARK Socket Server has been started.")
+
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.load_cert_chain(certfile="crt/cert-20170526-150709.crt", keyfile="crt/key-20170526-150709.pem")
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Socket created")
+with open("Transactions.txt", "a") as w1:
+    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Socket created")
+
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print("Bind failed. Error Code : " + str(msg[0]) + " Message " + msg[1])
+    with open("Transactions.txt", "a") as w1:
+        w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Bind failed. Error Code : " + str(msg[0]) + " Message " + msg[1])
+    sys.exit()
+
+print("Socket bind complete")
+with open("Transactions.txt", "a") as w1:
+    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Socket bind complete")
+
+s.listen(1)
+print("Socket now listening")
+with open("Transactions.txt", "a") as w1:
+    w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Socket now listening")
+
+
+while 1:
+
+    newsocket, fromaddr = s.accept()
+    print("Connected with " + fromaddr[0] + ":" + str(fromaddr[1]))
+    with open("Transactions.txt", "a") as w1:
+        w1.write("\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Connected with " + fromaddr[0] + ":" + str(fromaddr[1]))
+    connstream = context.wrap_socket(newsocket, server_side=True, do_handshake_on_connect=True)
+    _thread.start_new_thread(clientthread ,(connstream,))
+s.close()
