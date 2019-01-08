bdb90a18fe81321c037464490768ed19b1f9c3e3
ZeroDelayy <nilsfv@gmx.co.uk>
2019-01-08 22:00:33 +0000

    Defined environment for Message class

---
+++ libserver.py
@@ -0,0 +1,19 @@
+import sys
+import selectors
+import json
+import io
+import struct
+
+
+class Message:
+    def __init__(self, selector, sock, addr):
+        self.selector = selector
+        self.sock = sock
+        self.addr = addr
+        self._recv_buffer = b""
+        self._send_buffer = b""
+        self._jsonheader_len = None
+        self.jsonheader = None
+        self.request = None
+        self.response_created = False
+
