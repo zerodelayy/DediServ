b716f31f30b093d9eff02b964f94197e0e396817
ZeroDelayy <nilsfv@gmx.co.uk>
2019-01-09 22:55:24 +0000

    Started libclient.

---
+++ libclient.py
@@ -0,0 +1,42 @@
+import sys
+import selectors
+import json
+import io
+import struct
+
+
+class Message:
+    def __init__(self, selector, sock, addr, request):
+        self.selector = selector
+        self.sock = sock
+        self.addr = addr
+        self.request = request
+        self._recv_buffer = b""
+        self._send_buffer = b""
+        self._request_queued = False
+        self._jsonheader_len = None
+        self.jsonheader = None
+        self.response = None
+
+    def _set_selector_events_mask(self, mode):
+        if mode == "r":
+            events = selectors.EVENT_READ
+        elif mode == "w":
+            events = selectors.EVENT_WRITE
+        elif mode == "rw":
+            events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        else:
+            raise ValueError(f"Invalid events mask mode {repr(mode)}.")
+        self.selector.modify(self.sock, events, data=self)
+
+    def _read(self):
+        try:
+            data = self.sock.recv(4096)
+        except BlockingIOError:
+            pass
+        else:
+            if data:
+                self._recv_buffer =+ data
+            else:
+                raise RuntimeError("Connection to Peer closed.")
+
