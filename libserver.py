85fb5ef1b52fbd8df4de25cea438d7f717c8aad4
ZeroDelayy <nilsfv@gmx.co.uk>
2019-01-09 21:11:30 +0000

    Building up the libserver file by reverse engineering a socket server tutorial.

--- libserver.py
+++ libserver.py
@@ -28,4 +28,27 @@ class Message:
             raise ValueError(f"Invalid events mask mode {repr(mode)}.")
         self.selector.modify(self.sock, events, data=self)
 
+    def _read(self):
+        try:
+            data = self.sock.recv(4096)
+        except BlockingIOError:
+            pass
+        else:
+            if data:
+                self._recv_buffer += data
+            else:
+                raise RuntimeError("Peer connection has been closed.")
+
+    def _write(self):
+        if self._send_buffer:
+            print("Sending {0} to {1}".format(self._send_buffer, self.addr))
+            try:
+                sent = self.sock.send(self._send_buffer)
+            except BlockingIOError:
+                pass
+            else:
+                self._send_buffer = self._send_buffer[sent:]
+                if sent and not self._send_buffer:
+                    self.close()
+
 
